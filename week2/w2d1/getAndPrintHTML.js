73c8bcad24a5e7bc806f399a6cfda23cbadf9c2d
Thofeeq <thofeeqm@gmail.com>
2018-09-03 19:53:30 +0000

    Working with buffers to recieve stream data

--- week2/w2d1/getAndPrintHTML.js
+++ week2/w2d1/getAndPrintHTML.js
@@ -1,18 +1,23 @@
 var https = require('https');
-function getAndPrintHTMLChunks () {
-
+var stringBuffer = "";
+function getAndPrintHTML () {
+  
   var requestOptions = {
     host: 'sytantris.github.io',
-    path: '/http-examples/step1.html'
+    path: '/http-examples/step2.html'
   };
+
+
   https.get(requestOptions, function(response){
     //setting the encoding of recieved data
     response.setEncoding('utf8');
     //using 'data' to recieve and logging the recieved chunks to the console
     response.on('data', function(data){
-      console.log(data.toString() + '\n');
+      stringBuffer += data;
     });
+    response.on('end',function(){console.log(stringBuffer);} )
   });
-}
+  
+} 
 
-getAndPrintHTMLChunks();
+getAndPrintHTML();
